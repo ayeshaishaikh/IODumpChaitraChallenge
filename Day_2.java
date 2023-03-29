4eaee52d5c1a62d7fdf6070648cde7660f122071
ayeshaishaikh <87683724+ayeshaishaikh@users.noreply.github.com>
2023-03-29 12:48:40 +0530

    Create Day_2.java

---
+++ Day_2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Day_2
+{
+    public static void main(String args[])
+    {
+        char ch = 'g';
+
+        if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
+            System.out.println(ch + " is vowel");
+        
+        else
+            System.out.println(ch + " is constant");
+    }
+}
